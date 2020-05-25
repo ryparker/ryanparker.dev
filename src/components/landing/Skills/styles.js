e59b1908b0ede59147d7b2f39eb08d71f64390d1
Ryan Parker <ryan.parker3@outlook.com>
2020-05-24 22:50:17 -0500

    Added interactions to particles background

--- src/components/landing/Skills/styles.js
+++ src/components/landing/Skills/styles.js
@@ -1,11 +1,11 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const Wrapper = styled.div`
 	background-image: url('../illustrations/details.svg');
 	background-size: contain;
 	background-position: left top;
 	background-repeat: no-repeat;
-`;
+`
 
 export const SkillsWrapper = styled.div`
 	padding: 4rem 0;
@@ -16,7 +16,7 @@ export const SkillsWrapper = styled.div`
 	@media (max-width: 960px) {
 		flex-direction: column;
 	}
-`;
+`
 
 export const Details = styled.div`
 	flex: 1;
@@ -40,33 +40,21 @@ export const Details = styled.div`
 		line-height: 1.3;
 		color: #707070;
 	}
-`;
+`
 
-export const StyledImage = styled.img.attrs(_ => {
-	return {};
+export const StyledImage = styled.img.attrs((_) => {
+	return {}
 })`
 	padding: 4rem 0;
-`;
+`
 
-export const SkillTitle = styled.h3.attrs(_ => {
-	return {'data-sal': 'slide-left', 'data-sal-easing': 'ease'};
-})``;
+export const SkillTitle = styled.h3.attrs((_) => {
+	return { 'data-sal': 'slide-left', 'data-sal-easing': 'ease' }
+})``
 
-// Export const SkillList = styled.ul.attrs(props => {
-//   return { 'data-sal': 'slide-left', 'data-sal-easing': 'ease' }
-// })`
-//   li {
-//     margin-bottom: 1.5rem;
-//     font-size: 15pt;
-//     font-weight: normal;
-//     line-height: 1.3;
-//     color: #707070;
-//   }
-// `
-
-export const Skill = styled.li.attrs(_ => {
-	return {'data-sal': 'slide-left', 'data-sal-easing': 'ease'};
-})``;
+export const Skill = styled.li.attrs((_) => {
+	return { 'data-sal': 'slide-left', 'data-sal-easing': 'ease' }
+})``
 
 export const Thumbnail = styled.div`
 	flex: 1;
@@ -83,4 +71,4 @@ export const Thumbnail = styled.div`
 
 		width: 100%;
 	}
-`;
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
-import {Layout, Seo} from 'Common';
-import {Contact, Intro, Projects, Skills} from 'Components/landing';
-import React from 'react';
-import Particles from 'react-particles-js';
+import { Layout, Seo } from 'Common'
+import { Contact, Intro, Projects, Skills } from 'Components/landing'
+import React from 'react'
+import Particles from 'react-particles-js'
+
+import particleConfig from './particle-config.json'
 
 export default () => (
-	<Layout style={{position: 'absolute', height: '100%', width: '100%'}}>
-		<Seo/>
+	<Layout style={{ position: 'absolute', height: '100%', width: '100%' }}>
+		<Seo />
 		<Particles
 			style={{
 				position: 'fixed',
@@ -16,116 +18,13 @@ export default () => (
 				top: 0,
 				left: 0,
 				zIndex: '-99',
-				overflow: 'auto'
+				overflow: 'auto',
 			}}
-			params={{
-				particles: {
-					number: {
-						value: 20,
-						density: {
-							enable: true,
-							value_area: 800
-						}
-					},
-					color: {
-						value: '#6C63FF'
-					},
-					shape: {
-						type: 'circle',
-						stroke: {
-							width: 0,
-							color: '#000000'
-						},
-						polygon: {
-							nb_sides: 5
-						}
-					},
-					opacity: {
-						value: 0.5,
-						random: false,
-						anim: {
-							enable: false,
-							speed: 1,
-							opacity_min: 0.1,
-							sync: false
-						}
-					},
-					size: {
-						value: 3,
-						random: true,
-						anim: {
-							enable: false,
-							speed: 80,
-							size_min: 0.1,
-							sync: false
-						}
-					},
-					line_linked: {
-						enable: true,
-						distance: 300,
-						color: '#6C63FF',
-						opacity: 0.4,
-						width: 2
-					},
-					move: {
-						enable: true,
-						speed: 2,
-						direction: 'bottom',
-						random: false,
-						straight: false,
-						out_mode: 'out',
-						bounce: false,
-						attract: {
-							enable: false,
-							rotateX: 600,
-							rotateY: 1200
-						}
-					}
-				},
-				interactivity: {
-					detect_on: 'canvas',
-					events: {
-						onhover: {
-							enable: false,
-							mode: 'repulse'
-						},
-						onclick: {
-							enable: false,
-							mode: 'push'
-						},
-						resize: true
-					},
-					modes: {
-						grab: {
-							distance: 800,
-							line_linked: {
-								opacity: 1
-							}
-						},
-						bubble: {
-							distance: 800,
-							size: 80,
-							duration: 2,
-							opacity: 0.8
-						},
-						repulse: {
-							distance: 400,
-							duration: 0.4
-						},
-						push: {
-							particles_nb: 4
-						},
-						remove: {
-							particles_nb: 2
-						}
-					}
-				},
-				retina_detect: true
-			}}
+			params={particleConfig}
 		/>
-		<Intro/>
-		<Projects/>
-		<Skills/>
-		<Contact/>
+		<Intro />
+		<Projects />
+		<Skills />
+		<Contact />
 	</Layout>
-);
+)
